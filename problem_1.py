40781a1bd7eef202aec2a58ab8136058566aa4a9
aditya-raghav-69 <aditya09969@gmail.com>
2024-09-11 14:30:56 +0530

    Problem 1 printed teh ables from 2 to 20

---
+++ problem_1.py
@@ -0,0 +1,12 @@
+# printing tables from 2 to 20 in a separate file
+
+# printing tables from 2 to 20 in a separate file
+
+path = "C:/Users/pc/Desktop/GitDemo/PythonRepo1/tables.txt"
+
+with open(path, "a+", encoding="utf-8") as f:
+    for i in range(2,20):
+        for  j in range(1,11):
+            s = f"          {i}X{j}    =    {i*j}  \n "
+            f.write(s)
+        f.write('\n')
